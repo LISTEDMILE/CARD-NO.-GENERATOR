8a345ac2954440e02f5d5d51cee4d399fb1ccd7f
LISTEDMILE <listedmile@gmail.com>
2024-06-16 02:43:16 +0530

    GO

---
+++ tool.js
@@ -0,0 +1,10 @@
+//hiding unhiding div of diff inputs...._____
+function next(){
+    document.getElementById('mid1').style.display="none";
+    document.getElementById('mid2').style.display="flex";
+}
+
+function previous(){
+    document.getElementById('mid2').style.display="none";
+    document.getElementById('mid1').style.display="flex";
+}
