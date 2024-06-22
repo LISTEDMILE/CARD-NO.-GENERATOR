1a22469b8d0377b36f2da1d8ddf69af5a144b51c
LISTEDMILE <listedmile@gmail.com>
2024-06-22 09:07:53 +0530

    GO

--- tool.js
+++ tool.js
@@ -1,35 +1,60 @@
 //hiding unhiding div of diff inputs...._____
 var l=1;
+var number;
+
+var cartype="";
+var carcat="";
+var carbank="";
+anst="";
+
 function next(){
+
+    number=cartype+carcat+carbank+anst;
     if(l==5){
         l=0;
     }
     l=l+1;
     if(l==1){
-      
+      number="";
        document.getElementById('mid1').style.display="flex";
        document.getElementById('mid5').style.display="none";
      
        document.getElementById('finaldiv').style="border:none; padding:0px 0px";
+       document.querySelectorAll('.finaldiv')[0].innerHTML="";
+       document.querySelectorAll('.finaldiv')[1].innerHTML="";
+
 
     }
+    
+
     else if (l==2){
     document.getElementById('mid1').style.display="none";
     document.getElementById('mid2').style.display="flex";
+
+    document.querySelectorAll('.finaldiv')[0].innerHTML="number";
+    document.querySelectorAll('.finaldiv')[1].innerHTML="number";
     }
     else if (l==3){
         document.getElementById('mid2').style.display="none";
   
     document.getElementById('mid3').style.display="flex";
+
+    document.querySelectorAll('.finaldiv')[0].innerHTML="number";
+    document.querySelectorAll('.finaldiv')[1].innerHTML="number";
   
     }
     else if(l==4){
         
     document.getElementById('mid3').style.display="none";
   document.getElementById('mid4').style.display="flex";
+
+  document.querySelectorAll('.finaldiv')[0].innerHTML="number";
+  document.querySelectorAll('.finaldiv')[1].innerHTML="number";
+  
   
 }
 else {
+    anst=toString(document.getElementById('an').innerHTML.value);
     document.getElementById('mid4').style.display="none";
   document.getElementById('mid5').style.display="flex";
   
@@ -57,9 +82,7 @@ function previous(){
   
     }
 }
-var cartype;
-var carcat;
-var carbank;
+
 function cartypeh(){
     document.getElementById('cartypeh').innerHTML=cartype;
     document.getElementById('cartypecardh').innerHTML=cartype;
