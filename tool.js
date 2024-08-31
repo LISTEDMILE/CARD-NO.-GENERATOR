4a36ee9d72aa07b42dee0bdedb753f471104f6e2
LISTED MILE <listedmile@gmail.com>
2024-09-01 01:08:45 +0530

    GO

--- tool.js
+++ tool.js
@@ -1,6 +1,7 @@
 //hiding unhiding div of diff inputs...._____
-var l=1;
+var l=0;
 var number;
+var method1;
 
 var cartype="";
 var carcat="";
@@ -10,31 +11,50 @@ anst="";
 function next(){
 
     number=cartype+carcat+carbank+anst;
-    if(l==5){
-        l=0;
-    }
+  
+   
     l=l+1;
-    if(l==1){
+   
+   if(l==1){
+
+    if (method1==1){
+        l=4;
+        document.getElementById('mid0').style.display="none";
+        document.getElementById('mid4').style.display="flex";
+        document.getElementById('card').style.display="none";
+    }
+    else if(method1==2){
+       
       number="";
        document.getElementById('mid1').style.display="flex";
-       document.getElementById('mid5').style.display="none";
+       document.getElementById('mid0').style.display="none";
      
        document.getElementById('finaldiv').style="border:none; padding:0px 0px";
        document.querySelectorAll('.finaldiv')[0].innerHTML="";
        document.querySelectorAll('.finaldiv')[1].innerHTML="";
-
+   }
+    else{
+        alert("Please select a value");
+        l=0;
+    }
 
     }
     
 
     else if (l==2){
+        if(cartype!=""){
     document.getElementById('mid1').style.display="none";
     document.getElementById('mid2').style.display="flex";
 
     document.querySelectorAll('.finaldiv')[0].innerHTML="number";
     document.querySelectorAll('.finaldiv')[1].innerHTML="number";
-    }
+    } 
+    else{
+        l--;
+        alert("Please select one or use Bin no.")
+    }}
     else if (l==3){
+        if(carcat!=""){
         document.getElementById('mid2').style.display="none";
   
     document.getElementById('mid3').style.display="flex";
@@ -42,29 +62,49 @@ function next(){
     document.querySelectorAll('.finaldiv')[0].innerHTML="number";
     document.querySelectorAll('.finaldiv')[1].innerHTML="number";
   
-    }
-    else if(l==4){
-        
-    document.getElementById('mid3').style.display="none";
-  document.getElementById('mid4').style.display="flex";
+    } else{
+        l--;
+        alert("Please select one or use Bin no.")
+    }}
 
+
+    else if(l==4){
+if(carbank!=""){
+        l++;
+    document.getElementById('mid3').style.display="none";
+  document.getElementById('mid5').style.display="flex";
   document.querySelectorAll('.finaldiv')[0].innerHTML="number";
   document.querySelectorAll('.finaldiv')[1].innerHTML="number";
   
   
-}
+} else{
+        l--;
+        alert("Please select one or use Bin no.")
+    }}
 else {
     anst=toString(document.getElementById('an').innerHTML.value);
     document.getElementById('mid4').style.display="none";
   document.getElementById('mid5').style.display="flex";
+
   
     document.getElementById('finaldiv').style="border-right:2px solid rgba(45, 215, 238, 0.74); border-bottom:2px solid rgba(45, 215, 238, 0.74); padding:8px 20px";
 }
 }
 
 function previous(){
+    if(l==4){
+        l=0;
+        document.getElementById('card').style.display="flex";
+        document.getElementById('mid4').style.display="none";
+    document.getElementById('mid0').style.display="flex";
+    }
+    else{
     l=l-1;
-    if(l==1){
+    if(l==0){
+        document.getElementById('mid1').style.display="none";
+        document.getElementById('mid0').style.display="flex";
+        }
+    else if(l==1){
     document.getElementById('mid2').style.display="none";
     document.getElementById('mid1').style.display="flex";
     }
@@ -73,6 +113,7 @@ function previous(){
     document.getElementById('mid2').style.display="flex";
     }
     else if(l==3){
+
     document.getElementById('mid4').style.display="none";
     document.getElementById('mid3').style.display="flex";
     }
@@ -81,8 +122,14 @@ function previous(){
   document.getElementById('mid4').style.display="flex";
   
     }
+}}
+function method(){
+    if (method1==1){
+    document.getElementById('method').innerHTML="Bin";
 }
-
+else{
+    document.getElementById('method').innerHTML="Cridentials";
+}}
 function cartypeh(){
     document.getElementById('cartypeh').innerHTML=cartype;
     document.getElementById('cartypecardh').innerHTML=cartype;
@@ -94,4 +141,21 @@ function carcath(){
 function carbankh(){
     document.getElementById('carbankh').innerHTML=carbank;
     document.getElementById('insidebankh').innerHTML=carbank;
+}
+
+
+
+
+
+
+
+
+
+//logic.......
+
+function logic(){
+    var parentobj=new Map{
+        ['visa':['credit':['OBC Bank':45]]]
+    };
+    console.log(parentobj.'visa'.'credit'.'OBC Bank'.value);
 }
